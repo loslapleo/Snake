0f725223eb367bb00497beae052a3028de2cd79f
Louis Venter <venter.leoh@gmail.com>
2023-07-13 09:55:12 +0200

    Can now move like a snake

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-﻿
-namespace Snake
+﻿namespace Snake
 {
+	using System;
+	using System.Timers;
+
     class Program
 	{
 		const int X_SIZE = 80;
 		const int Y_SIZE = 20;
 		const int SIZE = X_SIZE * Y_SIZE;
+		static Timer timer = new Timer();
+		enum Direction {
+			Up,
+			Down,
+			Left,
+			Right
+		}
+		static Direction direction = Direction.Right;
+		static int[,] grid = new int[X_SIZE, Y_SIZE];
+		static int[,] snake = new int[SIZE, 2];
+		static int score = 0;
+		static int appleX = Convert.ToInt32(X_SIZE * .25);
+		static int appleY = Convert.ToInt32(Y_SIZE * .25);
 
 		public static void Main(string[] args)
 		{
@@ -13,65 +28,35 @@ namespace Snake
 			Console.Title = "Game";
 			Console.CursorVisible = false;
 
-			int[,] grid = new int[X_SIZE, Y_SIZE];
-
-			int[,] snake = new int[SIZE, 2];
 			snake[0, 0] = Convert.ToInt32(X_SIZE * .5);
 			snake[0, 1] = Convert.ToInt32(Y_SIZE * .5);
-			int appleX = Convert.ToInt32(X_SIZE * .25);
-			int appleY = Convert.ToInt32(Y_SIZE * .25);
-			int score = 0;
 
-			while (quit == false) {
-				Console.Clear();
-				for (int i = 0; i < score + 1; i++) {
-					Console.SetCursorPosition(snake[i, 0], snake[i, 1]);
-					Console.Write("o");
-				}
-				Console.SetCursorPosition(appleX, appleY);
-				Console.Write("@");
-				Console.SetCursorPosition(0, Y_SIZE);
-				Console.Write("Score: " + score);
+			timer.Interval = 128;
+			timer.Elapsed += Tick;
+			timer.AutoReset = true;
+			timer.Enabled = true;
 
+			while (!quit) {
 				ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 				switch (keyInfo.Key) {
 					case ConsoleKey.Escape:
 						quit = true;
 						break;
 					case ConsoleKey.UpArrow:
-						if (snake[0, 1] > 0) {
-							snake = slither(snake);
-							snake[0, 1]--;
-						}
+						direction = Direction.Up;
 						break;
 					case ConsoleKey.DownArrow:
-						if (snake[0, 1] < Y_SIZE - 1) {
-							snake = slither(snake);
-							snake[0, 1]++;
-						}
+						direction = Direction.Down;
 						break;
 					case ConsoleKey.LeftArrow:
-						if (snake[0, 0] > 0) {
-							snake = slither(snake);
-							snake[0, 0]--;
-						}
+						direction = Direction.Left;
 						break;
 					case ConsoleKey.RightArrow:
-						if (snake[0, 0] < X_SIZE - 1) {
-							snake = slither(snake);
-							snake[0, 0]++;
-						}
+						direction = Direction.Right;
 						break;
 					default:
 						// Unrecognized key was used.
 						break;
-				}
-
-				if (snake[0, 0] == appleX && snake[0, 1] == appleY) {
-					Random random = new Random();
-					appleX = random.Next(0, X_SIZE);
-					appleY = random.Next(0, Y_SIZE);
-					score++;
 				}
 			}
 
@@ -79,7 +64,57 @@ namespace Snake
 			Console.CursorVisible = true;
 		}
 
-		private static int[,] slither(int[,] snake)
+		private static void Tick(Object source, ElapsedEventArgs e)
+		{
+			Console.Clear();
+			for (int i = 0; i < score + 1; i++) {
+				Console.SetCursorPosition(snake[i, 0], snake[i, 1]);
+				Console.Write("o");
+			}
+			Console.SetCursorPosition(appleX, appleY);
+			Console.Write("@");
+			Console.SetCursorPosition(0, Y_SIZE);
+			Console.Write("Score: " + score);
+
+			switch (direction) {
+				case Direction.Up:
+					if (snake[0, 1] > 0) {
+						snake = Slither(snake);
+						snake[0, 1]--;
+					}
+					break;
+				case Direction.Down:
+					if (snake[0, 1] < Y_SIZE - 1) {
+						snake = Slither(snake);
+						snake[0, 1]++;
+					}
+					break;
+				case Direction.Left:
+					if (snake[0, 0] > 0) {
+						snake = Slither(snake);
+						snake[0, 0]--;
+					}
+					break;
+				case Direction.Right:
+					if (snake[0, 0] < X_SIZE - 1) {
+						snake = Slither(snake);
+						snake[0, 0]++;
+					}
+					break;
+			}
+
+			if (snake[0, 0] == appleX && snake[0, 1] == appleY) {
+				Random random = new Random();
+				appleX = random.Next(0, X_SIZE);
+				appleY = random.Next(0, Y_SIZE);
+				score++;
+			}
+
+			Console.SetCursorPosition(0, Y_SIZE + 1);
+			Console.Write(e.SignalTime);
+		}
+
+		private static int[,] Slither(int[,] snake)
 		{
 			for (int i = SIZE - 1; i > 0; i--) {
 				snake[i, 0] = snake[i - 1, 0];
